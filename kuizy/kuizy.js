032a8939fb0e85d73e342cb438fc853ddd210691
china331mi <chinami.andreia@gmail.com>
2021-08-02 19:00:52 +0900

    正解文を加えた

--- kuizy/kuizy.js
+++ kuizy/kuizy.js
@@ -5,7 +5,6 @@ document.getElementById('success').addEventListener('click', () => {
     document.getElementById('fail1').classList.add('cantclick');
     document.getElementById('fail2').classList.add('cantclick');
     document.getElementById('succcess').classList.add('cantclick');
-
 });
 
 document.getElementById('fail1').addEventListener('click', () => {
@@ -21,3 +20,16 @@ document.getElementById('fail2').addEventListener('click', () => {
     document.getElementById('success').classList.add('cantclick');
     document.getElementById('fail2').classList.add('cantclick');
 });
+
+document.getElementById('quizresult').style.display="none";
+function clickanswer(){
+    const quizresult = document.getElementById('quizresult');
+
+    if(quizresult.style.display=="block"){
+        quizresult.style.display="none";
+    }else{
+        quizresult.style.display="block";
+    }
+}
+
+
